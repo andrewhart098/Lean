27390ad09fd0bf19704402d16fd39a9bcba05d1e
andrewhart098 <andrewthart098@gmail.com>
2016-08-29 10:31:46 -0400

    Nothing

---
+++ UserInterface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuantConnect.Views.WinForms;
+
+namespace QuantConnect.Views
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new Exception(
+                    "Error: You must specify the port on which the application will open a TCP socket.");
+            }
+
+            var port = args[0];
+
+            var desktopApi = new DesktopMessageHandler(port);
+            var form = new LeanWinForm(desktopApi);
+
+            desktopApi.ReceivedJobEvent += (packet) =>
+            {
+                form.Initialize(packet);
+            };
+
+
+            Application.Run(form);
+        }
+    }
+}
