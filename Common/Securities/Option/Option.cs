16d63fab9568d2190675232a746ad7ee917c6ce7
Andrew Hart <andrewthart098@gmail.com>
2016-11-09 14:40:11 -0500

    ImmediateFillModel uses actual ask and bid prices for tick data. SpreadSlippageModel removed

--- Common/Securities/Option/Option.cs
+++ Common/Securities/Option/Option.cs
@@ -54,7 +54,7 @@ namespace QuantConnect.Securities.Option
                 new SecurityPortfolioModel(),
                 new ImmediateFillModel(),
                 new InteractiveBrokersFeeModel(),
-                new SpreadSlippageModel(),
+                new ConstantSlippageModel(0), 
                 new ImmediateSettlementModel(),
                 Securities.VolatilityModel.Null,
                 new SecurityMarginModel(2m),
