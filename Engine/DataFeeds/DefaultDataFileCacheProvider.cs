a16d39ff1e505e9717b4da9b0b9c6f6d2e47ff72
Andrew <andrewthart098@gmail.com>
2017-02-10 16:36:56 -0500

    ZipFiles effectively disposed

--- Engine/DataFeeds/DefaultDataFileCacheProvider.cs
+++ Engine/DataFeeds/DefaultDataFileCacheProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Ionic.Zip;
 using QuantConnect.Data;
 using QuantConnect.Interfaces;
 using QuantConnect.Lean.Engine.DataFeeds.Transport;
@@ -8,21 +9,26 @@ namespace QuantConnect.Lean.Engine.DataFeeds
 {
     public class DefaultDataFileCacheProvider : IDataFileCacheProvider
     {
+        private ZipFile _zipFile;
+
         public Stream Fetch(string source, DateTime date, string entryName)
         {
            return source.GetExtension() == ".zip"
-                ? Compression.UnzipBaseStream(source, entryName)
+                ? Compression.UnzipBaseStream(source, entryName, out _zipFile)
                 : new FileStream(source, FileMode.Open, FileAccess.Read);
         }
 
         public void Store(string source, byte[] data)
         {
-            //
+            // NOP
         }
 
         public void Dispose()
         {
-            //
+            if (_zipFile != null)
+            {
+                _zipFile.Dispose();
+            }
         }
     }
 }
